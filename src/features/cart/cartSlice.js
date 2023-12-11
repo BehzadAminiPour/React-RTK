9b3bc49c1510f5f2ff1d31e1d8f5a3f2c24cbaf0
Behzad Aminipour <behzadaminipour87@gmail.com>
2023-12-11 18:39:42 +0330

    run mini project

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,73 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
+
+const initialState = {
+  cartItems: [],
+  amount: 10,
+  total: 0,
+  isLoading: true,
+};
+
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios(url);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("Something went wrong");
+    }
+  }
+);
+
+const cartSlice = createSlice({
+  name: "cart ",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotal: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount = item.amount + amount;
+        total = item.amount * item.price + total;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.cartItems = action.payload;
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+export const { clearCart, removeItem, increase, decrease, calculateTotal } =
+  cartSlice.actions;
+export default cartSlice.reducer;
